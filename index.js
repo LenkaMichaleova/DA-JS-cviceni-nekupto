6c6362fdc25debd72739f7d3aaaac831e49c3e7d
Lenka Michaleová <michaleova.lenka@gmail.com>
2025-03-26 20:25:12 +0100

    vyřešení druhé části úkolu podle zadání na kodim.cz - první část je zakomentovaná, aby nový kód fungoval!

--- index.js
+++ index.js
@@ -16,12 +16,50 @@ const product3 = {
   description: 'Aby muži na rozpálených plážích kromě vašeho těla obdivovali také vaše nitro.',
 };
 
-const card1 = document.querySelector("#product1")
-const btn2 = document.querySelector("#product2 .btn")
-const cardTitle = document.querySelector("#product3 .card-title")
+// ------- 1. část úkolu ---------- //
+// const card1 = document.querySelector("#product1")
+// const btn2 = document.querySelector("#product2 .btn")
+// const cardTitle = document.querySelector("#product3 .card-title")
 
-card1.classList.add("border-primary")
-btn2.classList.remove("btn-primary")
-cardTitle.classList.toggle("text-center")
-cardTitle.classList.toggle("text-center")
-cardTitle.classList.toggle("text-center")
+// card1.classList.add("border-primary")
+// btn2.classList.remove("btn-primary")
+// cardTitle.classList.toggle("text-center")
+// cardTitle.classList.toggle("text-center")
+// cardTitle.classList.toggle("text-center")
+
+
+// -------- 2. část úkolu ----------//
+const product1HTML = `
+  <img class="card-img-top" src="${product1.image}" alt="Card image cap">
+  <div class="card-body">
+    <h2 class="card-title">${product1.name}</h2>
+    <p class="card-text">${product1.description}</p>
+  </div>
+  <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>
+`
+
+const product2HTML = `
+  <img class="card-img-top" src="${product2.image}" alt="Card image cap">
+  <div class="card-body">
+    <h2 class="card-title">${product2.name}</h2>
+    <p class="card-text">${product2.description}</p>
+  </div>
+  <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>
+`
+
+const product3HTML = `
+  <img class="card-img-top" src="${product3.image}" alt="Card image cap">
+  <div class="card-body">
+    <h2 class="card-title">${product3.name}</h2>
+    <p class="card-text">${product3.description}</p>
+  </div>
+  <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>
+`
+
+const product1Obsah = document.querySelector("#product1")
+const product2Obsah = document.querySelector("#product2")
+const product3Obsah = document.querySelector("#product3")
+
+product1Obsah.innerHTML = product1HTML
+product2Obsah.innerHTML = product2HTML
+product3Obsah.innerHTML = product3HTML
